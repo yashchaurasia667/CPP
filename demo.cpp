245408eb879c16221439e6599a5a5b4cf5e3dbdd
yashchaurasia667 <yashchaurasia630@gmail.com>
2024-01-25 13:30:48 +0530

    initial commit

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello, World!" << endl;
+
+    int amt1;
+    cin >> amt1;
+
+    int amt2;
+    cin >> amt2;
+
+    int sum = amt1 + amt2;
+    cout << sum;o
+    return 0;
+}
---
+++ if-else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int savings;
+    cin >> savings;
+
+    if (savings > 7000)
+        cout << "damnn nigga!!" << endl;
+    else if (savings > 2000)
+        cout << "nigga!!" << endl;
+    else
+        cout << "nigga broke!!" << endl;
+
+    return 0;
+}
